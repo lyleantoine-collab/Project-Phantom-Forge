9b0cdae9c82433141ee9343baed038b1b4f2c510
Lyle Antoine <lyleantoine@gmail.com>
2026-01-13 13:57:01 -0330

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+st.title('Project Phantom Forge') 
+st.markdown(You've passed the disclaimer. What do you want to do?) 
+if st.button('Start Bilateral Stimulation'): 
+st.write('Coming soon — eye dot guide.') 
+else: 
+st.write('Waiting.')
